bb01460b2efc71af13076e4ddc03e3ed2ef9ce4a
jjh030325 <wpqlks7@gmail.com>
2023-06-02 01:49:08 +0900

    에임게임 기능추가

--- WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.Designer.cs
@@ -32,6 +32,9 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.scoreLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer_label = new System.Windows.Forms.Label();
+            this.shootcount_txt = new System.Windows.Forms.Label();
+            this.accuracy_txt = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,27 +42,70 @@
             // 
             this.scoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.scoreLabel.AutoSize = true;
-            this.scoreLabel.Location = new System.Drawing.Point(1585, 33);
+            this.scoreLabel.Location = new System.Drawing.Point(1214, 9);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(54, 18);
+            this.scoreLabel.Size = new System.Drawing.Size(45, 15);
             this.scoreLabel.TabIndex = 0;
             this.scoreLabel.Text = "label1";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(45, 86);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.aimgame_background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 67);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1616, 596);
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // timer_label
+            // 
+            this.timer_label.AutoSize = true;
+            this.timer_label.Location = new System.Drawing.Point(129, 28);
+            this.timer_label.Name = "timer_label";
+            this.timer_label.Size = new System.Drawing.Size(68, 15);
+            this.timer_label.TabIndex = 2;
+            this.timer_label.Text = "timer : 30";
+            // 
+            // shootcount_txt
+            // 
+            this.shootcount_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.shootcount_txt.AutoSize = true;
+            this.shootcount_txt.Location = new System.Drawing.Point(1214, 28);
+            this.shootcount_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.shootcount_txt.Name = "shootcount_txt";
+            this.shootcount_txt.Size = new System.Drawing.Size(105, 15);
+            this.shootcount_txt.TabIndex = 3;
+            this.shootcount_txt.Text = "shootcount : 0";
+            // 
+            // accuracy_txt
+            // 
+            this.accuracy_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.accuracy_txt.AutoSize = true;
+            this.accuracy_txt.Location = new System.Drawing.Point(1064, 28);
+            this.accuracy_txt.Name = "accuracy_txt";
+            this.accuracy_txt.Size = new System.Drawing.Size(91, 15);
+            this.accuracy_txt.TabIndex = 4;
+            this.accuracy_txt.Text = "명중률 : 0 %";
+            // 
             // aimtrainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1710, 708);
+            this.ClientSize = new System.Drawing.Size(1368, 590);
+            this.Controls.Add(this.accuracy_txt);
+            this.Controls.Add(this.shootcount_txt);
+            this.Controls.Add(this.timer_label);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.scoreLabel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "aimtrainForm";
             this.Text = "aimtrainForm";
             this.Load += new System.EventHandler(this.aimtrainForm_Load);
@@ -74,5 +120,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label timer_label;
+        private System.Windows.Forms.Label shootcount_txt;
+        private System.Windows.Forms.Label accuracy_txt;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.cs
@@ -14,19 +14,29 @@ namespace WindowsFormsApp1
     {
         private Random random;
         private int score;
+        private int shootcount;
+        private double accuracy;
         private PictureBox crosshair;
         private PictureBox target;
+
+        private int counttime = 30;
+        private Timer play_timer;     //타이머 추가
+        private int elapsedTime; //경과시간
+        private bool startcheck = false;
+
         public aimtrainForm()
         {
             InitializeComponent();
             this.WindowState = FormWindowState.Maximized;
             random = new Random();
             score = 0;
+            shootcount = 0;
             crosshair = new PictureBox();
-            crosshair.BackColor = Color.Red;
+            crosshair.SizeMode = PictureBoxSizeMode.Zoom;
             crosshair.Size = new Size(20, 20);
             crosshair.Visible = true;
             pictureBox1.Controls.Add(crosshair); // pictureBox에 crosshair를 추가
+            crosshair.Image = Properties.Resources.aim_image; 
 
             target = new PictureBox();
             target.BackColor = Color.Blue;
@@ -35,8 +45,27 @@ namespace WindowsFormsApp1
             pictureBox1.Controls.Add(target); // pictureBox에 target을 추가
             target.Click += target_Click; // target의 클릭 이벤트 핸들러 추가
             pictureBox1.MouseMove += pictureBox_MouseMove; // pictureBox1의 마우스 이동 이벤트 핸들러 추가
-        }
+            target.MouseMove += target_MouseMove;
+            crosshair.Click += crosshair_Click;
 
+            // 타이머 초기화
+            play_timer = new Timer();
+            play_timer.Interval = 1000; // 1초마다 타이머 이벤트 발생
+            play_timer.Tick += Timer_Tick;
+            elapsedTime = 0;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++; // 경과 시간 증가
+            timer_label.Text = "timer : " + (counttime - elapsedTime).ToString(); // 경과 시간 표시
+
+            if (counttime - elapsedTime <= 0)
+            {
+                play_timer.Stop(); // 타이머 멈춤
+                pictureBox1.Controls.Remove(target); // target PictureBox 제거
+                MessageBox.Show("time over");
+            }
+        }
         private void aimtrainForm_Load(object sender, EventArgs e)
         {
             // 게임 초기화
@@ -46,8 +75,7 @@ namespace WindowsFormsApp1
         private void ResetGame()
         {
             score = 0;
-            scoreLabel.Text = "Score: 0";
-            timer1.Start();
+            scoreLabel.Text = "Score : 0";
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -59,14 +87,26 @@ namespace WindowsFormsApp1
         }
         private void target_Click(object sender, EventArgs e)
         {
-            // 타겟 클릭 이벤트 핸들러
-            score++;
-            scoreLabel.Text = "Score: " + score.ToString();
+            if (counttime - elapsedTime > 0)
+            {
+                // 타겟 클릭 이벤트 핸들러
+                score++;
+                shootcount++;
+                scoreLabel.Text = "Score : " + score.ToString();
+                shootcount_txt.Text = "Shootcount : " + shootcount.ToString();
+                accuracy = (double)score / shootcount * 100;
+                accuracy_txt.Text = "명중률 : " + accuracy.ToString("0.00") + " %";
+                if (startcheck == false)
+                {
+                    startcheck = true;
+                    play_timer.Start();
+                }
 
-            // target의 위치를 랜덤하게 이동
-            int x = random.Next(pictureBox1.Width - target.Width);
-            int y = random.Next(pictureBox1.Height - target.Height);
-            target.Location = new Point(x, y);
+                // target의 위치를 랜덤하게 이동
+                int x = random.Next(pictureBox1.Width - target.Width);
+                int y = random.Next(pictureBox1.Height - target.Height);
+                target.Location = new Point(x, y);
+            }
         }
         private void pictureBox_MouseMove(object sender, MouseEventArgs e)
         {
@@ -74,6 +114,32 @@ namespace WindowsFormsApp1
             int x = e.X - crosshair.Width / 2;
             int y = e.Y - crosshair.Height / 2;
             crosshair.Location = new Point(x, y);
+        }
+
+        private void target_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X - crosshair.Width / 2;
+            int y = e.Y - crosshair.Height / 2;
+            crosshair.Location = new Point(target.Left + x, target.Top + y);
+        }
+
+        private void crosshair_Click(object sender, EventArgs e)
+        {
+            if (counttime - elapsedTime > 0)
+            {
+                if (crosshair.Bounds.IntersectsWith(target.Bounds))
+                {
+                    // 겹쳐져 있다면 target을 클릭한 것으로 처리
+                    target_Click(target, EventArgs.Empty);
+                }
+                else if (startcheck == true)
+                {
+                    shootcount++;
+                    shootcount_txt.Text = "Shootcount : " + shootcount.ToString();
+                    accuracy = (double)score / shootcount * 100;
+                    accuracy_txt.Text = "명중률 : " + accuracy.ToString("0.00") + " %";
+                }
+            }
         }
     }
 }
