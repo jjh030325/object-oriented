2a0cf63601ad1c3d085e6820e84125feb0ab1d42
jjh030325 <wpqlks7@gmail.com>
2023-06-02 04:17:07 +0900

    mini fix

--- WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/aimtrainForm.Designer.cs
@@ -43,10 +43,10 @@
             this.scoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.scoreLabel.AutoSize = true;
             this.scoreLabel.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreLabel.Location = new System.Drawing.Point(1320, 34);
+            this.scoreLabel.Location = new System.Drawing.Point(1056, 28);
             this.scoreLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(140, 35);
+            this.scoreLabel.Size = new System.Drawing.Size(117, 30);
             this.scoreLabel.TabIndex = 0;
             this.scoreLabel.Text = "Score : 0";
             // 
@@ -57,10 +57,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.aimgame_background;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(98, 80);
+            this.pictureBox1.Location = new System.Drawing.Point(78, 67);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1500, 540);
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 450);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -69,10 +69,9 @@
             // 
             this.timer_label.AutoSize = true;
             this.timer_label.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer_label.Location = new System.Drawing.Point(92, 34);
-            this.timer_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timer_label.Location = new System.Drawing.Point(74, 28);
             this.timer_label.Name = "timer_label";
-            this.timer_label.Size = new System.Drawing.Size(154, 35);
+            this.timer_label.Size = new System.Drawing.Size(131, 30);
             this.timer_label.TabIndex = 2;
             this.timer_label.Text = "timer : 30";
             // 
@@ -81,10 +80,10 @@
             this.shootcount_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.shootcount_txt.AutoSize = true;
             this.shootcount_txt.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.shootcount_txt.Location = new System.Drawing.Point(450, 34);
+            this.shootcount_txt.Location = new System.Drawing.Point(360, 28);
             this.shootcount_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shootcount_txt.Name = "shootcount_txt";
-            this.shootcount_txt.Size = new System.Drawing.Size(240, 35);
+            this.shootcount_txt.Size = new System.Drawing.Size(205, 30);
             this.shootcount_txt.TabIndex = 3;
             this.shootcount_txt.Text = "shootcount : 0";
             // 
@@ -93,18 +92,17 @@
             this.accuracy_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.accuracy_txt.AutoSize = true;
             this.accuracy_txt.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.accuracy_txt.Location = new System.Drawing.Point(896, 31);
-            this.accuracy_txt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.accuracy_txt.Location = new System.Drawing.Point(717, 26);
             this.accuracy_txt.Name = "accuracy_txt";
-            this.accuracy_txt.Size = new System.Drawing.Size(225, 35);
+            this.accuracy_txt.Size = new System.Drawing.Size(191, 30);
             this.accuracy_txt.TabIndex = 4;
             this.accuracy_txt.Text = "Accuracy : 0 %";
             // 
             // aimtrainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1710, 708);
+            this.ClientSize = new System.Drawing.Size(1368, 590);
             this.Controls.Add(this.accuracy_txt);
             this.Controls.Add(this.shootcount_txt);
             this.Controls.Add(this.timer_label);
